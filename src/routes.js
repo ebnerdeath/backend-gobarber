cb9f5799f9a3c79210b3b8d1754b9ba464c42a86
Ebner Matias Trigo da Silva <Ciffrabuy@iMac-de-Carlos.local>
2019-08-17 16:37:30 -0300

    Configures first persist in Users

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
+import User from './app/models/User';
+
 const { Router } = require('express');
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Ebner Silva',
+    email: 'ebner.suporte@hotmail.com',
+    password_hash: '123123',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
