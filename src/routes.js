f2eb28a4d7dac4c1981806f7c855fdd1dc7542f2
ebnerdeath <ebner.suporte@hotmail.com>
2019-08-23 23:35:06 -0300

    Feature/Added relationship to model User and File

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,17 @@
 import multer from 'multer';
-import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+
+import multerConfig from './config/multer';
 
 import authMiddleware from './app/middlewares/auth';
+
+const upload = multer(multerConfig);
 
 const { Router } = require('express');
 
 const routes = new Router();
-const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 
@@ -17,8 +20,6 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
